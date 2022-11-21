0e466ff7b5fea69614536e0bb26245bac0e3253e
oye-nifemi <nifoyewole@gmail.com>
2022-11-21 09:00:11 +0100

    PAU Cafe Project

---
+++ src/cafeteria/project/CafeteriaProject.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cafeteria.project;
+
+/**
+ *
+ * @author nifoy
+ */
+public class CafeteriaProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
